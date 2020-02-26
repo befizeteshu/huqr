924676e62d021b1d428faefc10bf63fa0c46c8b5
Gabor Dolla <agdolla@gmail.com>
2020-02-26 17:07:31 +0100

    wip

---
+++ src/huqr.ts
@@ -0,0 +1,137 @@
+const Joi = require('@hapi/joi');
+
+const huChars = /^[\x20-\x7eáíűőüöúóéÁÍŰŐÜÖÚÓÉ]*$/;
+
+const schema = Joi.object({
+  kind: Joi
+    .string()
+    .valid('HCT', 'RTP')
+    .required(),
+  version: Joi
+    .string()
+    .valid('001')
+    .required(),
+  charset: Joi
+    .string()
+    .valid('1')
+    .required(),
+  bic: Joi
+    .string()
+    .pattern(/^[0-9]+$/, 'numbers')
+    .length(11)
+    .required(),
+  name: Joi
+    .string()
+    .pattern(huChars)
+    .max(70)
+    .required(),
+  iban: Joi           // TODO implement iban checksum
+    .string()
+    .pattern(/^HU[0-9]+$/, 'iban')
+    .length(28)
+    .required(),
+  amount: Joi
+    .string()
+    .pattern(/^HUF[0-9]+$/)
+    .min(4)
+    .max(15),
+  validUntil: Joi
+    .string()
+    .pattern(/^[0-9]+\+[0-9]$/) // TODO more validation ? valid date, not in past
+    .length(16)
+    .required(),
+  purpose: Joi // TODO purpose checker ?
+    .string()
+    .pattern(huChars)
+    .length(4),
+  message: Joi
+    .string()
+    .pattern(huChars)
+    .max(70),
+  shopId: Joi
+    .string()
+    .pattern(huChars)
+    .max(35),
+  merchDevId: Joi
+    .string()
+    .pattern(huChars)
+    .max(35),
+  invoiceId: Joi
+    .string()
+    .pattern(huChars)
+    .max(35),
+  customerId: Joi
+    .string()
+    .pattern(huChars)
+    .max(35),
+  credTranId: Joi
+    .string()
+    .pattern(huChars)
+    .max(35),
+  loyaltyId: Joi
+    .string()
+    .pattern(huChars)
+    .max(35),
+  navCheckId: Joi
+    .string()
+    .pattern(huChars)
+    .max(35)
+});
+
+// the order is important !
+const  props = ['kind', 'version', 'charset', 'bic', 'name', 'iban', 'amount', 'validUntil', 'purpose',
+  'message', 'shopId', 'merchDevId', 'invoiceId', 'customerId', 'credTranId', 'loyaltyId', 'navCheckId'];
+
+export default class MNBQrCode {
+  kind?: 'HCT' | 'RTP';
+  version?: '001';
+  charset?: '1';
+  bic?: string;
+  name?: string;
+  iban?: string;
+  amount?: string;
+  validUntil?: string;
+  purpose?: string;
+  message?: string;
+  shopId?: string;
+  merchDevId?: string;
+  invoiceId?: string;
+  customerId?: string;
+  credTranId?: string;
+  loyaltyId?: string;
+  navCheckId?: string;
+
+  [key: string]: any;
+
+  validate() {
+    return schema.validate(this);
+  }
+
+  isValid() {
+    return this.validate().error === undefined;
+  }
+
+  read(value: string): boolean {
+    const lines = value.split(/\n/);
+    if (lines.length !== (props.length + 1)) {
+      // there is an LF after the last line so split returns an array with one more element
+      return false;
+    }
+    for (let i=0; i<props.length; i++) {
+      if (lines[i] !== '') {
+        this[props[i]] = lines[i];
+      }
+    }
+    return true;
+  }
+
+  toString() {
+    let result = '';
+    for (let i=0; i<props.length; i++) {
+      result += this[props[i]] ?? '';
+      result +=  '\n';
+    }
+    // TODO send warning if full length is over 345 chars
+    return result;
+  }
+}
---
+++ test/library.test.ts
@@ -0,0 +1,251 @@
+import MNBQrCode from "../src/huqr";
+
+function getValidInstance() {
+  const code = new MNBQrCode();
+  code.kind = 'HCT';
+  code.version = '001';
+  code.charset = '1';
+  code.bic = '12345678901';
+  code.name = 'Test Elek';
+  code.iban = 'HU12345678901234567890123456';
+  code.validUntil = '20201231235959+2';
+  return code;
+}
+
+function expectError(code: MNBQrCode) {
+  expect(code.isValid()).toBe(false);
+}
+function expectNoError(code: MNBQrCode) {
+  expect(code.isValid()).toBe(true);
+}
+
+describe('joi definition test', () => {
+  it('MNBQrCode is instantiable', () => {
+    expect(new MNBQrCode()).toBeInstanceOf(MNBQrCode);
+  });
+  it('invalid after instantiation', () => {
+    const code = new MNBQrCode();
+    expectError(code);
+  });
+  it('code validation works', () => {
+    const code = getValidInstance();
+    code.kind = 'HCT';
+    expectNoError(code);
+    code.kind = 'RTP';
+    expectNoError(code);
+    code.kind = undefined;
+    expectError(code);
+  });
+  it('version validation works', () => {
+    const code = getValidInstance();
+    code.version = '001';
+    expectNoError(code);
+    code.version = undefined;
+    expectError(code);
+  });
+  it('charset validation works', () => {
+    const code = getValidInstance();
+    code.charset = '1';
+    expectNoError(code);
+    code.charset = undefined;
+    expectError(code);
+  });
+  it('bic validation works', () => {
+    const code = getValidInstance();
+    code.bic = '12345678901';
+    expectNoError(code);
+    code.bic = undefined;
+    expectError(code);
+    code.bic = '1233456789012';
+    expectError(code);
+    code.bic = '12334567890A';
+    expectError(code);
+    code.bic = '123345678901😊';
+    expectError(code);
+  });
+  it('name validation works', () => {
+    const code = getValidInstance();
+    code.name = 'Test Elek';
+    expectNoError(code);
+    code.name = 'árvíztűrő tükörfúrógép';
+    expectNoError(code);
+    code.name = 'ÁRVÍZTŰRŐ TÜKÖRFÚRÓGÉP';
+    expectNoError(code);
+    code.name = undefined;
+    expectError(code);
+    code.name = '123345678901233456789012334567890123345678901233456789012334567890123345678901';
+    expectError(code);
+    code.name = 'Hakan Şükür';
+    expectError(code);
+  });
+  it('iban validation works', () => {
+    const code = getValidInstance();
+    code.iban = 'HU12345678901234567890123456';
+    expectNoError(code);
+    code.iban = undefined;
+    expectError(code);
+    code.iban = '1234567890';
+    expectError(code);
+    // TODO implement iban checksum
+  });
+  it('amount validation works', () => {
+    const code = getValidInstance();
+    code.amount = 'HUF1500';
+    expectNoError(code);
+    code.amount = undefined;
+    expectNoError(code);
+    code.amount = '1234567890';
+    expectError(code);
+    code.amount = 'HUF1234567890123';
+    expectError(code);
+    code.amount = 'HUF';
+    expectError(code);
+  });
+  it('validUntil validation works', () => {
+    const code = getValidInstance();
+    code.validUntil = '20201231235959+2';
+    expectNoError(code);
+    code.validUntil = undefined;
+    expectError(code);
+    code.validUntil = '1234567890123456';
+    expectError(code);
+    code.validUntil = '12345678901234+A';
+    expectError(code);
+    // TODO check validity, read date, not in past
+  });
+  it('purpose validation works', () => {
+    const code = getValidInstance();
+    code.purpose = 'ABCD';
+    expectNoError(code);
+    code.purpose = undefined;
+    expectNoError(code);
+    code.purpose = '12345';
+    expectError(code);
+    code.purpose = '123';
+    expectError(code);
+    // TODO check for allowed values
+  });
+  it('message validation works', () => {
+    const code = getValidInstance();
+    code.message = 'ABCD';
+    expectNoError(code);
+    code.message = undefined;
+    expectNoError(code);
+    code.message = '12345678901234567890123456789012345678901234567890123456789012345678901';
+    expectError(code);
+    code.message = '❤️';
+    expectError(code);
+  });
+  it('shopId validation works', () => {
+    const code = getValidInstance();
+    code.shopId = 'ABCD';
+    expectNoError(code);
+    code.shopId = undefined;
+    expectNoError(code);
+    code.shopId = '123456789012345678901234567890123456';
+    expectError(code);
+    code.shopId = '❤️';
+    expectError(code);
+  });
+  it('merchDevId validation works', () => {
+    const code = getValidInstance();
+    code.merchDevId = 'ABCD';
+    expectNoError(code);
+    code.merchDevId = undefined;
+    expectNoError(code);
+    code.merchDevId = '123456789012345678901234567890123456';
+    expectError(code);
+    code.merchDevId = '❤️';
+    expectError(code);
+  });
+  it('invoiceId validation works', () => {
+    const code = getValidInstance();
+    code.invoiceId = 'ABCD';
+    expectNoError(code);
+    code.invoiceId = undefined;
+    expectNoError(code);
+    code.invoiceId = '123456789012345678901234567890123456';
+    expectError(code);
+    code.invoiceId = '❤️';
+    expectError(code);
+  });
+  it('customerId validation works', () => {
+    const code = getValidInstance();
+    code.customerId = 'ABCD';
+    expectNoError(code);
+    code.customerId = undefined;
+    expectNoError(code);
+    code.customerId = '123456789012345678901234567890123456';
+    expectError(code);
+    code.customerId = '❤️';
+    expectError(code);
+  });
+  it('credTranId validation works', () => {
+    const code = getValidInstance();
+    code.credTranId = 'ABCD';
+    expectNoError(code);
+    code.credTranId = undefined;
+    expectNoError(code);
+    code.credTranId = '123456789012345678901234567890123456';
+    expectError(code);
+    code.credTranId = '❤️';
+    expectError(code);
+  });
+  it('loyaltyId validation works', () => {
+    const code = getValidInstance();
+    code.loyaltyId = 'ABCD';
+    expectNoError(code);
+    code.loyaltyId = undefined;
+    expectNoError(code);
+    code.loyaltyId = '123456789012345678901234567890123456';
+    expectError(code);
+    code.loyaltyId = '❤️';
+    expectError(code);
+  });
+  it('navCheckId validation works', () => {
+    const code = getValidInstance();
+    code.navCheckId = 'ABCD';
+    expectNoError(code);
+    code.navCheckId = undefined;
+    expectNoError(code);
+    code.navCheckId = '123456789012345678901234567890123456';
+    expectError(code);
+    code.navCheckId = '❤️';
+    expectError(code);
+  });
+});
+
+describe('read test', () => {
+  it('can read from string', () => {
+    const input = 'HCT\n001\n1\n12345678901\nTest Elek\nHU12345678901234567890123456\nHUF1500\n20201231235959+2\n\n\n\n\n\n\n\n\nTEST\n';
+    const code = new MNBQrCode();
+    expect(code.read(input)).toBe(true);
+    expectNoError(code);
+    expect(code.kind).toEqual('HCT');
+    expect(code.version).toEqual('001');
+    expect(code.charset).toEqual('1');
+    expect(code.bic).toEqual('12345678901');
+    expect(code.name).toEqual('Test Elek');
+    expect(code.iban).toEqual('HU12345678901234567890123456');
+    expect(code.amount).toEqual('HUF1500');
+    expect(code.validUntil).toEqual('20201231235959+2');
+    expect(code.navCheckId).toEqual('TEST');
+  });
+  it('no read if number of lines is not 18', () => {
+    const input = 'HCT\n001\n1\n12345678901\nTest Elek\nHU12345678901234567890123456\nHUF1500\n20201231235959+2\n\n\n\n\n\n\n\n\n';
+    const code = new MNBQrCode();
+    expect(code.read(input)).toBe(false);
+    expectError(code);
+  });
+});
+
+describe('write test', () => {
+  it('can write to a string', () => {
+    const input = 'HCT\n001\n1\n12345678901\nTest Elek\nHU12345678901234567890123456\nHUF1500\n20201231235959+2\naaaa\n\n\n\n\n\n\n\n\n';
+    const code = new MNBQrCode();
+    expect(code.read(input)).toBe(true);
+    expectNoError(code);
+    const result = code.toString();
+    expect(result).toEqual(input);
+  });
+});
